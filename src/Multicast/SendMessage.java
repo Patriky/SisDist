b38bea8dafad637f779d4319d872205f6c44653a
Lincoln <linkskill@gmail.com>
2018-08-22 12:21:48 -0300

    fix

--- src/Multicast/SendMessage.java
+++ src/Multicast/SendMessage.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class SendMessage extends Thread {
+public class SendMessage {
     private MulticastSocket socket;
     private InetAddress group;
 
